7fdc16af8a71106926dc47b490ec4a888e34bfdb
arunshika96 <maryemalda20@gmail.com>
2024-08-07 13:30:01 +0530

    first

--- src/app/views/admin/admin.routes.ts
+++ src/app/views/admin/admin.routes.ts
@@ -1,10 +1,12 @@
 import {Routes} from "@angular/router";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {AdminComponent} from "./admin.component";
+import { ClientsComponent } from "./clients/clients.component";
 
 export const adminRoutes: Routes = [
 
     {path:'',component:AdminComponent, children: [
             {path:'',component:DashboardComponent},
+            {path:'clients',component:ClientsComponent},
         ]},
 ];
